ddef85ff510c8d7b7a44a65d7c20e2330c2b7332
Valmar Júnior <valmar.junior1@gmail.com>
2018-04-24 15:39:54 -0300

    Main e contextos definidos. pom.xml com as bibliotecas iniciais.

---
+++ src/main/java/com/crosoften/ChatOn.java
@@ -0,0 +1,33 @@
+package com.crosoften;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ChatOn {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run( ChatOn.class, args );
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+		return args -> {
+			
+			System.out.println( "Beans utilizados no projeto:" );
+			
+			String[] beansNames = applicationContext.getBeanDefinitionNames();
+			Arrays.sort( beansNames );
+			
+			for (String beanName : beansNames) {
+				System.out.println( beanName );
+			}
+		};
+	}
+}
