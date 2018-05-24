bdeaf5e358d07e2e77c94b38d146054dc734759f
Valmar Júnior <valmar.junior1@gmail.com>
2018-05-24 15:37:25 -0300

    Builder fix

--- src/main/java/com/crosoften/ChatOn.java
+++ src/main/java/com/crosoften/ChatOn.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
@@ -16,37 +18,42 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = {
-	ChatOn.class,
-	Jsr310JpaConverters.class
+        ChatOn.class,
+        Jsr310JpaConverters.class
 })
 @EnableConfigurationProperties({
-	FileStorageProperties.class
+        FileStorageProperties.class
 })
-public class ChatOn {
-	
-	@PostConstruct
-	public void init() {
-		//@TODO: Arrumar esse timezone.
-		TimeZone.setDefault( TimeZone.getTimeZone( "UTC" ) );
-	}
-	
-	
-	public static void main(String[] args) {
-		SpringApplication.run( ChatOn.class, args );
-	}
-	
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
-		return args -> {
-			
-			System.out.println( "Beans utilizados no projeto:" );
-			
-			String[] beansNames = applicationContext.getBeanDefinitionNames();
-			Arrays.sort( beansNames );
-			
-			for (String beanName : beansNames) {
-				System.out.println( beanName );
-			}
-		};
-	}
+public class ChatOn extends SpringBootServletInitializer {
+
+    @PostConstruct
+    public void init() {
+        //@TODO: Arrumar esse timezone.
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatOn.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+        return args -> {
+
+            System.out.println("Beans utilizados no projeto:");
+
+            String[] beansNames = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(beansNames);
+
+            for (String beanName : beansNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ChatOn.class);
+    }
 }
